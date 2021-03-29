0808bf9d359013e19192319822f5a8663f2d91d3
SAKET_SKG <75434797+Saketkumargautam@users.noreply.github.com>
2021-03-29 13:20:09 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//using namespace std;
+int main ()// first function to excuted the program/code
+{
+    // use either using namespace std (once at a time in code)/std::(it repeated)
+    //cout<<"hello world";  cout means value print in display /print in console
+    //and "hello world" --> string
+    std::cout<<"hello world";
+    return 0;
+}
+
+
